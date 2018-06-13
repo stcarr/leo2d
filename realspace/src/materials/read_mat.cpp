a5d8eb63f8cec56d5bca33fc268706263afb1512
Stephen Carr <stephencarr@g.harvard.edu>
2018-06-13 15:04:22 -0400

    Starting to seperate loaded_mat into smaller data objects, intra_data and inter_data. THIS VERSION WILL NOT COMPILEgit add src/materials/read_mat.cpp

--- realspace/src/materials/read_mat.cpp
+++ realspace/src/materials/read_mat.cpp
@@ -113,109 +113,136 @@ LoadedMat ReadMat::loadMat(std::string filename){
   fileIn.open(filename);
 
   LoadedMat outMat;
+  std::vector< LoadedIntraData > intra_data;
+  std::vector< LoadedInterData > inter_data;
 
-  string name;
-  array< array<double, 2>, 2> lattice;
-
-  int num_orbs;
-  vector< array<double, 3> > orb_pos;
-
-  int num_mono_couplings;
-  int big_R;
-  int max_R;
-  vector< vector< vector< vector<double> > > > intralayer_terms;
-
+  int num_intra_data;
+  int num_inter_data;
 
   string temp;
 
-  fileIn >> name;   // name
-  //printf("name = %s \n",name.c_str());
-  fileIn.ignore(256, '\n');  // end of line
-  fileIn.ignore(256, '\n');  // blank line
-  fileIn.ignore(256, '\n');  // begin unit cell
+  // Get number of data types in this file
+  fileIn >> temp;
+  num_intra_data = atoi(temp.c_str());
+  intra_data.resize(num_intra_data)
 
   fileIn >> temp;
-  lattice[0][0] = atof(temp.c_str());
-  fileIn >> temp;
-  lattice[0][1] = atof(temp.c_str());
-  fileIn.ignore(256, '\n');
+  num_inter_data = atoi(temp.c_str());
+  inter_data.resize(num_inter_data)
 
-  fileIn >> temp;
-  lattice[1][0] = atof(temp.c_str());
-  fileIn >> temp;
-  lattice[1][1] = atof(temp.c_str());
-  fileIn.ignore(256, '\n');
+  // loop over intra data (comes  before inter data)
+  for (int idx = 0; idx < num_intra_data; ++idx){
+    string name;
+    array< array<double, 2>, 2> lattice;
 
-  //printf("lattice = [%lf, %lf ; %lf, %f] \n",lattice[0][0],lattice[0][1],lattice[1][0],lattice[1][1]);
+    int num_orbs;
+    vector< array<double, 3> > orb_pos;
 
-  fileIn.ignore(256, '\n');  // c axis
-  fileIn.ignore(256, '\n');  // end unit cell
-  fileIn.ignore(256, '\n');  // blank line
+    int num_mono_couplings;
+    int big_R;
+    int max_R;
+    vector< vector< vector< vector<double> > > > intralayer_terms;
 
-  fileIn.ignore(256, '\n');  // begin orb list
+    fileIn >> name;   // name
+    //printf("name = %s \n",name.c_str());
+    fileIn.ignore(256, '\n');  // end of line
+    fileIn.ignore(256, '\n');  // blank line
+    fileIn.ignore(256, '\n');  // begin unit cell
 
-  fileIn >> num_orbs;
-  //printf("num_orbs = %d \n",num_orbs);
-  fileIn.ignore(256, '\n');  // end of line
+    fileIn >> temp;
+    lattice[0][0] = atof(temp.c_str());
+    fileIn >> temp;
+    lattice[0][1] = atof(temp.c_str());
+    fileIn.ignore(256, '\n');
 
-  // Now read the positions of every orbital
-  orb_pos.resize(num_orbs);
-  for (int i = 0; i < num_orbs; ++i){
-    fileIn >> orb_pos[i][0];
-    fileIn >> orb_pos[i][1];
-    fileIn >> orb_pos[i][2];
-    //printf("orb_pos[%d] = [%lf, %lf, %lf] \n",i,orb_pos[i][0],orb_pos[i][1],orb_pos[i][2]);
-  }
+    fileIn >> temp;
+    lattice[1][0] = atof(temp.c_str());
+    fileIn >> temp;
+    lattice[1][1] = atof(temp.c_str());
+    fileIn.ignore(256, '\n');
 
-  fileIn.ignore(256, '\n');  // end of line
-  fileIn.ignore(256, '\n');  // end orb list
-  fileIn.ignore(256, '\n');  // blank line
+    //printf("lattice = [%lf, %lf ; %lf, %f] \n",lattice[0][0],lattice[0][1],lattice[1][0],lattice[1][1]);
 
-  fileIn.ignore(256, '\n');  // begin  mono couplings
-  fileIn >> num_mono_couplings;
-  fileIn >> big_R;
-  fileIn.ignore(256, '\n');  // end of line
-  max_R = big_R*2 + 1;
+    fileIn.ignore(256, '\n');  // c axis
+    fileIn.ignore(256, '\n');  // end unit cell
+    fileIn.ignore(256, '\n');  // blank line
 
-  // resize intralyer_terms array
-  intralayer_terms.resize(max_R);
-  for (int r_i = 0; r_i < max_R; ++r_i){
-    intralayer_terms[r_i].resize(max_R);
-    for (int r_j = 0; r_j < max_R; ++r_j){
-      intralayer_terms[r_i][r_j].resize(num_orbs);
-      for (int o1 = 0; o1 < num_orbs; ++o1){
-        intralayer_terms[r_i][r_j][o1].resize(num_orbs);
+    fileIn.ignore(256, '\n');  // begin orb list
+
+    fileIn >> num_orbs;
+    //printf("num_orbs = %d \n",num_orbs);
+    fileIn.ignore(256, '\n');  // end of line
+
+    // Now read the positions of every orbital
+    orb_pos.resize(num_orbs);
+    for (int i = 0; i < num_orbs; ++i){
+      fileIn >> orb_pos[i][0];
+      fileIn >> orb_pos[i][1];
+      fileIn >> orb_pos[i][2];
+      //printf("orb_pos[%d] = [%lf, %lf, %lf] \n",i,orb_pos[i][0],orb_pos[i][1],orb_pos[i][2]);
+    }
+
+    fileIn.ignore(256, '\n');  // end of line
+    fileIn.ignore(256, '\n');  // end orb list
+    fileIn.ignore(256, '\n');  // blank line
+
+    fileIn.ignore(256, '\n');  // begin  mono couplings
+    fileIn >> num_mono_couplings;
+    fileIn >> big_R;
+    fileIn.ignore(256, '\n');  // end of line
+    max_R = big_R*2 + 1;
+
+    // resize intralyer_terms array
+    intralayer_terms.resize(max_R);
+    for (int r_i = 0; r_i < max_R; ++r_i){
+      intralayer_terms[r_i].resize(max_R);
+      for (int r_j = 0; r_j < max_R; ++r_j){
+        intralayer_terms[r_i][r_j].resize(num_orbs);
+        for (int o1 = 0; o1 < num_orbs; ++o1){
+          intralayer_terms[r_i][r_j][o1].resize(num_orbs);
+        }
       }
     }
+
+    int R_i;
+    int R_j;
+    int o1;
+    int o2;
+    double t;
+
+    for (int i = 0; i < num_mono_couplings; ++i){
+
+      fileIn >> R_i;
+      fileIn >> R_j;
+      fileIn >> o1;
+      fileIn >> o2;
+      fileIn >> t;
+
+      intralayer_terms[R_i + big_R][R_j + big_R][o1][o2] = t;
+
+    }
+
+    intra_data[idx].name = name;
+    intra_data[idx].num_orbs = num_orbs;
+    intra_data[idx].lattice = lattice;
+    intra_data[idx].orb_pos = orb_pos;
+    intra_data[idx].intralayer_terms = intralayer_terms;
+
+    //printf("done with ReadMat::loadMat (%d orbs, %d couplings) \n",num_orbs, num_mono_couplings);
+
   }
 
-  int R_i;
-  int R_j;
-  int o1;
-  int o2;
-  double t;
-
-  for (int i = 0; i < num_mono_couplings; ++i){
-
-    fileIn >> R_i;
-    fileIn >> R_j;
-    fileIn >> o1;
-    fileIn >> o2;
-    fileIn >> t;
-
-    intralayer_terms[R_i + big_R][R_j + big_R][o1][o2] = t;
+  // Now loop over all inter data
+  for (int idx = 0; idx < num_inter_data; ++idx){
 
   }
-
-  outMat.name = name;
-  outMat.num_orbs = num_orbs;
-  outMat.lattice = lattice;
-  outMat.orb_pos = orb_pos;
-  outMat.intralayer_terms = intralayer_terms;
-
-  printf("done with ReadMat::loadMat (%d orbs, %d couplings) \n",num_orbs, num_mono_couplings);
 
   fileIn.close();
+
+  outMat.num_intra_data = num_intra_data;
+  outMat.num_inter_data = num_inter_data;
+  outMat.intra_data = intra_data;
+  outMat.inter_data = inter_data;
   return outMat;
 
 }
