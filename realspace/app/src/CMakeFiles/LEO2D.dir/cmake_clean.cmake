28f2307c23b488b2bb7852e9402b00fcdd304ec8
ZoeGOGOGO <zzy950709@gmail.com>
2019-03-20 13:59:04 -0400

    more tblg tests

--- realspace/app/src/CMakeFiles/LEO2D.dir/cmake_clean.cmake
+++ realspace/app/src/CMakeFiles/LEO2D.dir/cmake_clean.cmake
@@ -1,10 +1,10 @@
-file(REMOVE_RECURSE
+FILE(REMOVE_RECURSE
   "CMakeFiles/LEO2D.dir/main.cpp.o"
   "../LEO2D.pdb"
   "../LEO2D"
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/LEO2D.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/LEO2D.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
